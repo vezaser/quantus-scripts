ceccf975a63bdb86f91258806d23b0b2b8f2ddb8
Veza <159704021+vezaser@users.noreply.github.com>
2025-11-08 22:16:49 +0100

    Change node name from D02 to B01

--- run-node.sh
+++ run-node.sh
@@ -8,7 +8,7 @@ exec ./quantus-node \
   --base-path /root/quantus/chain_data_dir \
   --node-key-file /root/quantus/node-key \
   --rewards-address qzobRvuvMF8LDFwDa7ZuizaviNmVQNtGdQ6idq8jkFxt8tshy \
-  --name D02 \
+  --name B01 \
   --external-miner-url "http://127.0.0.1:9833" \
   --prometheus-port 9616 \
   --rpc-port 9944
